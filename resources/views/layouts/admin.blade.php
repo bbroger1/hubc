1bbfd57c178917b5ff0325bc12f1ecf6a9291ead
Rogerio Carvalho <bb.roger.bb@gmail.com>
2021-02-14 22:40:35 -0300

    instalação do argon dashboard

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }} - @yield('title')</title>
+
+    <!-- Styles -->
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('assets/css/panel/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="{{ asset('assets/css/panel/metisMenu.min.css') }}" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="{{ asset('assets/css/panel/timeline.css') }}" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('assets/css/panel/startmin.css') }}" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="{{ asset('assets/css/panel/morris.css') }}" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="{{ asset('assets/css/panel/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+
+    @stack('custom-css')
+</head>
+
+<body>
+    @yield('content')
+    <!-- Scripts -->
+    <!-- jQuery -->
+    <script src="{{ asset('assets/js/panel/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('assets/js/panel/bootstrap.min.js') }}"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="{{ asset('assets/js/panel/metisMenu.min.js') }}"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="{{ asset('assets/js/panel/startmin.js') }}"></script>
+    @stack('custom-js')
+</body>
+
+</html>
